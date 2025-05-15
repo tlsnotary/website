7abccf5ad6acfff3ba9364c881b4f0846d83f8aa
Hendrik Eeckhaut <hendrik@eeckhaut.org>
2025-05-15 10:54:55 +0200

    copy projects page from maci project

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -100,7 +100,10 @@ const config: Config = {
           position: 'left',
           label: 'Documentation',
         },
+
         { to: '/docs/faq', label: 'FAQ', position: 'left' },
+
+        { to: '/projects', label: 'Projects', position: 'left' },
 
         { href: 'https://tlsnotary.github.io/tlsn/tlsn_prover/', label: 'API', position: 'left' },
         { to: '/blog', label: 'Blog', position: 'left' },
---
+++ src/pages/projects.tsx
@@ -0,0 +1,44 @@
+import Layout from "@theme/Layout";
+import LayoutProvider from "@theme/Layout/Provider";
+import clsx from "clsx";
+
+import ProjectsList from "../components/ProjectList";
+
+import styles from "./index.module.css";
+
+interface ProjectspageHeaderProps {
+    tagline: string;
+    title: string;
+}
+
+const ProjectspageHeader = ({ tagline, title }: ProjectspageHeaderProps) => (
+    <header className={clsx("hero hero--dark", styles.heroBanner)}>
+        <div className={styles.heroTitle}>{title}</div>
+
+        <div className={styles.heroTagline}>
+            {tagline}
+
+            <span className={styles.blue}>.</span>
+        </div>
+    </header>
+);
+
+const Projects: React.FC = () => (
+    <LayoutProvider>
+        <Layout description="A list of projects built with the MACI protocol" title="Projects built with MACI">
+            <div>
+                <ProjectspageHeader tagline="Explore the projects built by the MACI community" title="Projects" />
+            </div>
+
+            <main>
+                <section className={styles.introduction}>
+                    <div className="container">
+                        <ProjectsList />
+                    </div>
+                </section>
+            </main>
+        </Layout>
+    </LayoutProvider>
+);
+
+export default Projects;
