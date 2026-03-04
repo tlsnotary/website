14facd4173a49927e4ac6c99341bc1bcd897ab40
Hendrik Eeckhaut <hendrik@eeckhaut.org>
2026-03-04 17:15:01 +0100

    Improved landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import Layout from '@theme/Layout';
 import HeroSection from './landing_page/_hero';
 import HowItWorksSection from './landing_page/_how-it-works';
 import WhySection from './landing_page/_why';
-import BuiltWithSection from './landing_page/_built-with';
 import CtaSection from './landing_page/_cta';
 
 export default function Home(): ReactNode {
@@ -16,7 +15,6 @@ export default function Home(): ReactNode {
       <main>
         <HowItWorksSection />
         <WhySection />
-        <BuiltWithSection />
         <CtaSection />
       </main>
     </Layout>
